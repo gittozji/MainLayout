644bcd52fc08e054a6e798269a94c0aca5014a4b
gittozji <yitozji@163.com>
2016-11-07 11:22:06 +0800

    精简

--- mainLayout.js
+++ mainLayout.js
@@ -1,13 +1,22 @@
-$(window).resize(function(){
-    if($(this).width() < 980){
+$(document).ready(function(){
+    //监听屏幕变化
+    $(window).resize(function(){
+        if($(this).width() < 980){
 
-    }
-});
+        }
+    });
 
-$(document).on("mouseover mouseout",".sidebar",function(e){
-    if(e.type == "mouseover"){
-        $(this).css("overflow","auto");
-    }else{
-        $(this).css("overflow","hidden");
-    }
+    //监听导航栏是否获取焦点
+    $(document).on("mouseover mouseout",".sidebar",function(e){
+        if(e.type == "mouseover"){
+            $(this).css("overflow","auto");
+        }else{
+            $(this).css("overflow","hidden");
+        }
+    })
+
+    //导航栏开关点击事件
+    $(".btn").click(function(){
+
+    })
 })
