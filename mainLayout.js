812b74954b7435dc55732adec47e2be96311d9b1
gittozji <yitozji@163.com>
2016-11-08 11:14:42 +0800

    回退到简单css设置动画的备份

--- mainLayout.js
+++ mainLayout.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
     //监听屏幕变化
     $(window).resize(function(){
         if($(this).width() < 980){
-
+            $(".main").css("marginLeft","0");
+            $(".sidebar").css("left","-250px");
+        }
+        else{
+            $(".main").css("marginLeft","250px");
+            $(".sidebar").css("left","0");
         }
     });
 
@@ -15,8 +20,26 @@ $(document).ready(function(){
         }
     })
 
-    //导航栏开关点击事件
+    //导航栏开启事件
     $(".btn").click(function(){
+        $(".screen").css({"width":"100%","height":"100%"});
+        openSidebar();
         alert("ddd");
     })
+
+    //导航栏关闭事件
+    $(".screen").click(function(){
+        $(".screen").css({"width":"0%","height":"0%"});
+        closeSidebar();
+    })
+
+    //打开导航栏
+    function openSidebar(){
+        $(".sidebar").removeClass(".mmd-sidebar-out");
+        $(".sidebar").addClass("mmd-sidebar-in");
+    }
+    //关闭导航栏
+    function closeSidebar(){
+        $(".sidebar").addClass("mmd-sidebar-out");
+    }
 })
